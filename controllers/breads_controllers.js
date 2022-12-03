aed2259537411459bb6386cd1ad81f0b0ef8cd43
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-03 11:42:01 -0800

    send index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -4,16 +4,25 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req, res) => {
-  res.render('Index',
-  {
-    breads: Bread,
-    title: 'Index Page'
-  }
-  )
+  Bread.find()
+    .then(foundBreads => {
+      res.render('index', {
+        breads: foundBreads,
+        title: 'Index Page'
+      }
+      )
+    })
+ 
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread: Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
 module.exports = breads
--- server.js
+++ server.js
@@ -9,15 +9,22 @@ const mongoose = require('mongoose')
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
   )
-  
+
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 const breadsController = require('./controllers/breads_controllers.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 
 
