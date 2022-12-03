1b91e82879f70d3bc57d1334017949f81873d8aa
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-03 12:18:24 -0800

    create and new routes complete

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
   )
