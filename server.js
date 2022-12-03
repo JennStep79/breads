a1013564c11765165c81873a9b3c9c4af29132ce
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-03 10:46:52 -0800

    add mongoose model

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const mongoose = require('mongoose')
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+  
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 const breadsController = require('./controllers/breads_controllers.js')
 app.use('/breads', breadsController)
+
 
 
 
