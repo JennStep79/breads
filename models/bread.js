7edadcb7ae51444e5f6348a23c1941f2d6648a70
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-05 18:54:30 -0800

    add baker schema validation

--- models/bread.js
+++ models/bread.js
@@ -6,7 +6,11 @@ const { Schema } = mongoose
 const breadSchema = new Schema ( {
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'https://images.unsplash.com/photo-1549931319-a545dcf3bc73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YnJlYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60' }
+  image: { type: String, default: 'https://images.unsplash.com/photo-1549931319-a545dcf3bc73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YnJlYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60' },
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+  }
 })
 
 const Bread = mongoose.model('Bread', breadSchema)
